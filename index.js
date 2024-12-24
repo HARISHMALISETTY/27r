cb1e87d7e1f5da236d708374af5c9457b1a1e4e9
HARISHMALISETTY <malisettyharish@gmail.com>
2024-12-24 10:54:25 +0530

    forloop

--- index.js
+++ index.js
@@ -1,163 +1,90 @@
-// let data = [
-//   {
-//     id: 1,
-//     title: "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
-//     price: 109.95,
-//     description:
-//       "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
-//     category: "men's clothing",
-//     image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
-//     rating: {
-//       rate: 3.9,
-//       count: 120,
-//     },
-//   },
-//   {
-//     id: 2,
-//     title: "Mens Casual Premium Slim Fit T-Shirts ",
-//     price: 22.3,
-//     category: "men's clothing",
-//     image:
-//       "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg",
-//     rating: {
-//       rate: 4.1,
-//       count: 259,
-//     },
-//   },
-//   {
-//     id: 3,
-//     title: "Mens Cotton Jacket",
-//     price: 55.99,
-//     description:
-//       "great outerwear jackets for Spring/Autumn/Winter, suitable for many occasions, such as working, hiking, camping, mountain/rock climbing, cycling, traveling or other outdoors. Good gift choice for you or your family member. A warm hearted love to Father, husband or son in this thanksgiving or Christmas Day.",
-//     category: "men's clothing",
-//     image: "https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg",
-//     rating: {
-//       rate: 4.7,
-//       count: 500,
-//     },
-//   },
-//   {
-//     id: 4,
-//     title: "Mens Casual Slim Fit",
-//     price: 15.99,
-//     description:
-//       "The color could be slightly different between on the screen and in practice. / Please note that body builds vary by person, therefore, detailed size information should be reviewed below on the product description.",
-//     category: "men's clothing",
-//     image: "https://fakestoreapi.com/img/71YXzeOuslL._AC_UY879_.jpg",
-//     rating: {
-//       rate: 2.1,
-//       count: 430,
-//     },
-//   },
-//   {
-//     id: 5,
-//     title:
-//       "John Hardy Women's Legends Naga Gold & Silver Dragon Station Chain Bracelet",
-//     price: 695,
-//     description:
-//       "From our Legends Collection, the Naga was inspired by the mythical water dragon that protects the ocean's pearl. Wear facing inward to be bestowed with love and abundance, or outward for protection.",
-//     category: "jewelery",
-//     image: "https://fakestoreapi.com/img/71pWzhdJNwL._AC_UL640_QL65_ML3_.jpg",
-//     rating: {
-//       rate: 4.6,
-//       count: 400,
-//     },
-//   },
-// ];
-// console.log(data[2].rating.rate)
+// // debugger;
+// // for (let i = 0; i <= 20; i = i + 2)
+// // {
+// //   console.log(i);
+// // }
 
-// // let arr=[1,2,3,4,5];
+// // 5X1=5
+// // 5X2=10
+// // 5X3=15
 
-// // console.log(arr[9])
+// // for (i = 10; i >= 1; i--) {
+// //   console.log(`5x${i}=${i * 5}`);
+// // }
+// // 1 10 1++  // increment
+// // 10 1 10-- // decrement
 
-// let isLogin = true;
 
-// let user = "admin";
+// let str="welcome";
+         
 
-// isLogin
-//   ? user == "customer"
-//     ? console.log("welcome to customer")
-//     : console.log("welcome to admin")
-//   : console.log("please login");
+// // for(i=2;i<=5;i++){
 
-// check user is vegetarian or nonvegiterian
+// //   console.log(str[i])
+// // }
+// // using for loop print each character line by line.
 
-// if veg--->check either gobi or panneer
-// 	if gobi-->print gobi
-// 	else print panner.
 
-// else---->check chicken or mutton.
-// 	if chicken-->print chicken
-// 	else print mutton.
-// let login = false;
-// let customer = "non";
-// let item = "chicken";
+// // let arr=[4,6,8,1,2,3,10];
 
-// login
-//   ? customer == "veg"
-//     ? item == "gobi"
-//       ? console.log("gobi")
-//       : console.log("paneer")
-//     : item == "chicken"
-//     ? console.log("chicken")
-//     : console.log("mutton")
-//   : console.log("please login");
+// // for(i=0;i<arr.length;i++)
+  
+// // {
 
-// debugger;
+// //   console.log(arr[i])
+// // }
 
-// console.log(a)
-// we are trying to access before intialisation---
-// in var case we will get undefined because only decleration part of the var will moves to
-// top of the scope.
-// console.log(b)
-//in the case of let and const, we can't access them before its initialisation
-// because they will be store in temporal dead zone until intialisation happens.
 
-// console.log(c)
+// 
 
-// var a=45;
+// harish
 
-// let b=5;
+// *
+// **
+// ***
+// ****
+// *****
 
-// const c=25;
 
-// console.log(a)
 
-// console.log(b)
+let str="harish";
 
-// console.log(c)
+let op="";
+// for(i=0;i<str.length;i++){
+// op+=str[i];
+// console.log(op)
 
-// let arr=["hi","hello","welcome","js","html","css"];
-
-// let str="javascript"
-// for(let i of str){
-
-//   console.log(i)
 // }
 
-// let aoo = [
-//   {
-//     id: 1,
-//     title: "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
-//     price: 109.95,
-//   },
-//   { id: 2, title: "Mens Casual Premium Slim Fit T-Shirts ", price: 22.3 },
-//   { id: 3, title: "Mens Cotton Jacket", price: 55.99 },
-// ];
 
-// for (i in aoo) {
-//   console.log(aoo[i].price);
+// for(i=0;i<5;i++){
+//   op+="*";
+//   console.log(op)
+  
+//   }
+
+
+let arr=["tharun","karthik","ganesh","rajaneesh"]
+
+// for(i=0;i<arr.length;i=i+2){
+//   console.log(arr[i])
 // }
 
-let arr=["aravind","suresh","karthik","ramya","Bhavya"];
-let a=1;
-for(var i in arr){
-  console.log(+i+1,"-",arr[i]);
+
+// for(i=0;i<arr.length;i++){
+//   console.log(arr[i][0])
+// }
+
+for(i=1;i<arr.length;i=i+2){
+  console.log("hello "+arr[i])
 }
-for(var i of arr){
-  console.log(a,"-",i);
-  a++;//a=a+1
-}
+
+
+// task:
+// -----
+// 1.check input string is palindrome or not? using for loop and terinary operator.
+// 2.i/p=["one","two","six","one"]---> o/p:n,w,i,n
+// 3.i/p=["one","two","three","four"]--->o/p:["eno","owt","eerht","ruof"];
+// notes on forloop vs for-in vs for-of
 
 
