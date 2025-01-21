a88ba1aeeec5839278a87b3c2443e3385cdaaa92
HARISHMALISETTY <malisettyharish@gmail.com>
2025-01-21 10:52:19 +0530

    array methods

---
+++ arraymethods.js
@@ -0,0 +1,101 @@
+// // // // // let arr=[];
+
+// // // // // arr[0]=1;
+// // // // // arr[1]=2;
+
+// // // // // arr[2]=4;
+
+// // // // // let arr=[1,2,3,4,6]
+
+// // // // let arr=new Array(1,2,3)
+// // // // console.log(arr)
+
+// // // // let arr=[1,2,3];
+
+// // // // console.log(arr)
+
+// // // // let op=[] // arr
+// // // // op=arr.toString(); // returning a string but not modifying existing array
+
+// // // // console.log(op)
+
+// // // let arr=[1,2,3,4];
+
+// // // // let op=arr.at(-3)
+// // // // console.log(op)
+
+// // // // console.log(arr.at(-3))
+
+// // // let op=arr.join(" hello ");
+// // // // console.log(op)
+
+
+// // // // join the elements in the array with seperator and returns in a string format.
+
+
+
+// // // // let arr2=[1,2,3,4,5];
+
+// // // // let op1=arr2.push(12);
+
+
+
+// // // // push method will modifies the existing array by adding one or more elements at the end of the array.
+// // // // and it will returns updated length of the array after adding elements.
+
+// // // // console.log(op1)
+
+// // // // let x=arr2.pop()
+
+// // // // console.log(x)
+
+// // // // pop method will removes last element from the array... and modifies existing array.
+// // // // it returns the element which is popped out.
+
+
+// // // let arr=[1,2,3,4,5];
+
+// // // let op=arr.shift()
+
+// // // console.log(op)
+
+// // // shift will modifies the existing array by removing the first element from the array
+// // //returns the element which is removed.
+
+
+// // let arr=[1,2,3,4,5];
+
+// // let op=arr.unshift(8,9,"hi","js",6,9,0)
+
+// // console.log(arr)
+
+// // // unshift will modifies the existing array by adding the elements at the starting of the array
+// // // and returns updated length of the array.
+
+// // let arr=["html","js","234",1,2,"react","nodejs"];
+
+// // let op=arr.slice(-2);
+
+// // console.log(op)
+
+// let arr=[1,2,3,4,5];
+
+// let x=arr.splice(1,6);
+
+// console.log(arr)
+
+// // splice will modifies existing array by removing/adding elements at the particular index in 
+// // the array.
+
+
+
+// let arr=[4,3,5,6,7,6,9,6];
+// let op=arr.indexOf(6,4);
+// console.log(op)
+
+
+let arr=[1,2,3,4,5,9];
+
+let op=arr.includes(4)
+
+console.log(op);
